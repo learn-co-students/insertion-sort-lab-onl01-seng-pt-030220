20f73d7fccdba8d60c6fe9204f8b85ca0623d102
anthonym5 <43191869+AnthonyM5@users.noreply.github.com>
2021-03-24 18:59:33 -0400

    Done.

--- index.js
+++ index.js
@@ -25,16 +25,3 @@ function selectionSort(array){
     return sorted;
 
 }
-
-function findMinAndRemove(array) {
-    let min = array[0];
-  let minIndex = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-      minIndex = i;
-    }
-  }
-  array.splice(minIndex, 1);
-  return min;
-}
